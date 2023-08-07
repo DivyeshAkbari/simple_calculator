de8524374843430520e6be082d5fd5eb7bb39fb4
Manan Padia <35602706+PadiaManan@users.noreply.github.com>
2023-08-07 15:36:50 -0400

    Create multiplication.py

---
+++ multiplication.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Multiplication
+multi_result = num1 * num2
+
+# Output
+print("The Multiplication of", num1, "and", num2, "is", multi_result)
