4080413312cf1fcc1a6f53170af7d0f5de3f0c64
unknown <divyeshakabari01@gmail.com>
2023-08-10 20:52:15 -0400

    Subtraction Removed!

--- subtraction.py
+++
@@ -1,8 +0,0 @@
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-
-# Addition
-sub_result = num1 - num2
-
-# Output
-print("The sub of", num1, "and", num2, "is", sub_result)
