6a68d729ee241469b56d86f6bae7ae348a044d7e
unknown <divyeshakabari01@gmail.com>
2023-08-10 20:49:53 -0400

    Addition Removed!

--- addition.py
+++
@@ -1,8 +0,0 @@
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-
-# Addition
-sum_result = num1 + num2
-
-# Output
-print("The sum of", num1, "and", num2, "is", sum_result)
